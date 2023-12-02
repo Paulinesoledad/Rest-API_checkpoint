7dbf35b03bcea49c17230cfe0727ce22a5079995
ominyitech <ominyipauline21@gmail.com>
2023-12-02 05:32:02 +0100

    updated

--- models/server.js
+++ models/server.js
@@ -13,7 +13,49 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {
 
 app.use(express.json());
 
-// Routes will be added later
+// GET: Return all users
+app.get('/users', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+  // POST: Add a new user to the database
+  app.post('/users', async (req, res) => {
+    const user = new User(req.body);
+    try {
+      const newUser = await user.save();
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+  // PUT: Edit a user by ID
+  app.put('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+      res.json(updatedUser);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+  
+  // DELETE: Remove a user by ID
+  app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+      await User.findByIdAndDelete(id);
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
